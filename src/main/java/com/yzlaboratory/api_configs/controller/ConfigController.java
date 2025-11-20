13ca1ce29aea7b3d962d34199913cb53bd2f77ba
Yannik Zeyer <yannik.zeyer@fmc-ag.com>
2025-11-20 13:10:20 +0100

    cors

--- src/main/java/com/yzlaboratory/api_configs/controller/ConfigController.java
+++ src/main/java/com/yzlaboratory/api_configs/controller/ConfigController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/configs")
+@CrossOrigin
 public class ConfigController {
 
     private final DynamoDbService dynamoDbService;
